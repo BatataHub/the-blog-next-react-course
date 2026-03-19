8bc427a5561cbef7bcda0120065fcb4cac6d8bf6
Norberto Gonçalves <norberto.goncalves@gmail.com>
2026-03-19 16:57:52 -0300

    aulas 112 ate 120

--- src/repositories/post/json-post-repository.ts
+++ src/repositories/post/json-post-repository.ts
@@ -11,8 +11,13 @@ const JSON_POST_FILE_PATH = resolve(
   'seed',
   'posts.json',
 );
+const SIMULATE_WAIT_IN_MS = 0;
 
 export class JsonPostRepository implements Postrepository {
+  private async simulateWait() {
+    return new Promise(resolve => setTimeout(resolve, SIMULATE_WAIT_IN_MS));
+  }
+
   private async readFromDisk() {
     const jsonContent = await readFile(JSON_POST_FILE_PATH, 'utf-8');
     const parseJson = JSON.parse(jsonContent);
@@ -21,10 +26,13 @@ export class JsonPostRepository implements Postrepository {
   }
 
   async findAll(): Promise<PostModel[]> {
+    await this.simulateWait();
+
     const posts = await this.readFromDisk();
     return posts;
   }
   async findById(id: string): Promise<PostModel | null> {
+    await this.simulateWait();
     const posts = await this.findAll();
     const post = posts.find(post => post.id === id);
 
